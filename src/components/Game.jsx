2486d6cf1ab3638e3b5339bd2f31d892e27ed4f5
BOUSSIF SAID <BOUSSIF.SAID@ofppt.ma>
2023-01-05 18:36:48 +0100

    deplacer assets to public, develope le code

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
 import '../components/Game.css';
-import De from '../assets/de.jpg' 
+// import De from '../assets/de.jpg' 
+// import face1 from '../assets/face1.png' 
+// import face2 from '../assets/face2.png' 
+// import face3 from '../assets/face3.png' 
+// import face4 from '../assets/face4.png' 
+// import face5 from '../assets/face5.png' 
+// import face6 from '../assets/face6.png' 
 
 class Game extends Component {
     constructor(props){
         super(props);
         this.state={face:null,computer:0,end:false }
     }
-    
+    Jouer = () =>{
+        const facevalue= Math.floor(Math.random()*6+1)
+        this.setState({face:facevalue,computer:this.state.computer+1,end:false })
+    }
+
   render() {
     return (
       <>
-      <img src={De} alt="" />
+      <img src={this.state.face==null
+        ?`assets/de.jpg`
+        :`assets/face${this.state.face}.png`
+        
+        
+        } alt="" />
       <h1>Jeu Dé</h1>
       <p>Face : {this.state.face}</p>
       <p>Nombre d'essais : {this.state.computer}</p>
-      <button onClick="">Jouer</button>
+      <button onClick={this.Jouer}>Jouer</button>
       </>
     )
   }
