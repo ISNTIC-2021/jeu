b808d7d2784ab8415e88f1baef9f2b8c49a808df
BOUSSIF SAID <BOUSSIF.SAID@ofppt.ma>
2023-01-05 11:23:30 +0100

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Game extends Component {
+  render() {
+    return (
+      <>
+      <img src="" alt="" />
+      <h1>Jeu Dé</h1>
+      <p>Face : </p>
+      <p>Nombre d'essais : </p>
+      <button>Jouer</button>
+      </>
+    )
+  }
+}
+
+
+
+export default Game;
