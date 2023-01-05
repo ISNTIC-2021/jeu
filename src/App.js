953d5f94dccd0c1490eb205584900bd83d03e661
BOUSSIF SAID <BOUSSIF.SAID@ofppt.ma>
2023-01-05 10:09:53 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,150 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state ={prenom:"Ali"};
+//   }
+//   changePrenom=()=>{
+//     this.setState({prenom:"Ahmed"})
+//   }
+//   render(){
+
+//     return(
+//       <>
+//       <h1>{this.state.prenom}</h1>
+//       <button onClick={this.changePrenom}>Change</button>
+      
+//       </>
+//     )
+//   }
+// }
+// export default App;
+
+
