be4e85e720226ce68b7eeab75b8c80d74881cfcf
BOUSSIF SAID <BOUSSIF.SAID@ofppt.ma>
2023-01-07 13:54:07 +0100

    some updates on  code

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
 import '../components/Game.css';
-// import De from '../assets/de.jpg' 
-// import face1 from '../assets/face1.png' 
-// import face2 from '../assets/face2.png' 
-// import face3 from '../assets/face3.png' 
-// import face4 from '../assets/face4.png' 
-// import face5 from '../assets/face5.png' 
-// import face6 from '../assets/face6.png' 
 
 class Game extends Component {
     constructor(props){
         super(props);
         this.state={face:null,computer:0,end:false }
     }
+   
     Jouer = () =>{
         
         const facevalue= Math.floor(Math.random()*6+1)
         this.setState({face:facevalue,computer:this.state.computer+1})
-        if (facevalue===3){
+        if (facevalue===8){
             this.setState({end:true})
         }
+
     }
 
     initial=()=>{
@@ -42,7 +37,6 @@ class Game extends Component {
         <button onClick={this.initial}>Initialiser</button>
         </>
         :<>
-          <p><input type="text"  placeholder='Une nombre entre 1 et 6' /></p>
           <button onClick={this.Jouer}>Jouer</button>
         </>
       }
